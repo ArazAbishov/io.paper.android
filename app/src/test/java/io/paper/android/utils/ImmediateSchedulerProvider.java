f62c039a60e40d1ea32cd8bfed4aa0420f53f5b0
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-19 12:20:51 +0100

    Changed implementation of the ImmediateSchedulerProvider.

--- app/src/test/java/io/paper/android/utils/ImmediateSchedulerProvider.java
+++ app/src/test/java/io/paper/android/utils/ImmediateSchedulerProvider.java
@@ -2,34 +2,29 @@ package io.paper.android.utils;
 
 import android.support.annotation.NonNull;
 
-import java.util.concurrent.Executor;
-
 import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
 
-
-public class ImmediateSchedulerProvider implements SchedulerProvider {
-    private final Executor currentThreadExecutor;
-
+public final class ImmediateSchedulerProvider implements SchedulerProvider {
     public ImmediateSchedulerProvider() {
-        this.currentThreadExecutor = Runnable::run;
+        // empty constructor
     }
 
     @NonNull
     @Override
     public Scheduler computation() {
-        return Schedulers.from(currentThreadExecutor);
+        return Schedulers.trampoline();
     }
 
     @NonNull
     @Override
     public Scheduler io() {
-        return Schedulers.from(currentThreadExecutor);
+        return Schedulers.trampoline();
     }
 
     @NonNull
     @Override
     public Scheduler ui() {
-        return Schedulers.from(currentThreadExecutor);
+        return Schedulers.trampoline();
     }
 }
