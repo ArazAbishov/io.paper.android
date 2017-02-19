72701beee288de3413f32f0e777b62337a9c992a
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-19 12:22:43 +0100

    Added test runner annotation to the NotesTest class.

--- app/src/test/java/io/paper/android/notes/NotesTest.java
+++ app/src/test/java/io/paper/android/notes/NotesTest.java
@@ -1,12 +1,15 @@
 package io.paper.android.notes;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
+@RunWith(JUnit4.class)
 public class NotesTest {
 
     @Test
