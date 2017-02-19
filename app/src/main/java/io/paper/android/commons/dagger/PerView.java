d13d9f06b5af14b95962981e823f9574afa63c06
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-19 14:07:37 +0100

    Moved @PerView scope under commons/dagger package.

--- app/src/main/java/io/paper/android/commons/dagger/PerView.java
+++ app/src/main/java/io/paper/android/commons/dagger/PerView.java
@@ -1,4 +1,4 @@
-package io.paper.android.ui;
+package io.paper.android.commons.dagger;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +7,5 @@ import javax.inject.Scope;
 
 @Scope
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ActivityScope {
+public @interface PerView {
 }
